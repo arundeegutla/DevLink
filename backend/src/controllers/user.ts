c607b6b9d4acc0fd8219de5c5263bc0825de6e6a
Usdiez <austinrobertson1010@gmail.com>
2023-10-18 18:17:40 -0400

    working getUser method

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -3,7 +3,8 @@ import { queryUserbyId } from "../services/user";
 import { User } from "../models/db";
 
 export const getUserbyId = async (req: Request, res: Response, next: NextFunction) => {
-    const queryID = req.params.slug;
+    const queryID: string = req.params.id;
+    console.log("SLUG", queryID);
     try {
         const userResult: (User|undefined) = await queryUserbyId(queryID);
         if (userResult === undefined)
