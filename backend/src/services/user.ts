17d104729019cee8550deac0f4dc0a900305e40f
MochaDev <michaelmcgarvey03@gmail.com>
2023-10-18 18:56:34 -0400

    Implement the user name search functionality

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -16,3 +16,21 @@ export const queryUserbyId = async (queryID: string): Promise<User | undefined> 
         throw error;
     }
 };
+
+export const queryUserbyName = async (queryName: string): Promise<User[] | undefined> => {
+    try {
+        queryName = queryName.toLowerCase();
+
+        const doc = await db.collection("Users")
+        .where("LowerFirstName", ">=", `${queryName}`)
+        .where("LowerFirstName", "<", `${queryName}` + "z")
+        .get();
+
+        const usersData = doc.docs.map(doc => doc.data() as User);
+
+        return usersData;
+    } catch (error) {
+        console.log("Error getting document:", error);
+        throw error;
+    }
+};
