cf00fc7af086f2c8e52bf825db27a601324e9315
Usdiez <austinrobertson1010@gmail.com>
2023-10-18 19:01:54 -0400

    removed

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -4,7 +4,6 @@ import { User } from "../models/db";
 
 export const getUserbyId = async (req: Request, res: Response, next: NextFunction) => {
     const queryID: string = req.params.id;
-    console.log("SLUG", queryID);
     try {
         const userResult: (User|undefined) = await queryUserbyId(queryID);
         if (userResult === undefined)
