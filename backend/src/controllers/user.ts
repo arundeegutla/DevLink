f3fc99b4f74f5060f3d5ab806ce2e13bba2bde26
Usdiez <austinrobertson1010@gmail.com>
2023-10-26 22:02:43 -0400

    added validation to create and edit profile

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { queryUserbyId, queryUserbyName, createProfile, editProfile } from "../services/user";
-import { User } from "../models/db";
+import { User, contactInfo } from "../models/db";
+import { validateNewUser, validateEdit } from "../utils/user";
 
 export const getUserbyId = async (req: Request, res: Response, next: NextFunction) => {
     const queryID: string = req.params.id;
@@ -26,8 +27,16 @@ export const getUserByName = async (req: Request, res: Response, next: NextFunct
   };
 
 export const createUserProfile = async (req: Request, res: Response, next: NextFunction) => {
-    const {firstName = "", lastName = "", contactInfo = {email: "", github: ""}, skills = []}: User = req.body;
-    const user: User = { firstName, lastName, contactInfo, skills, connections: [], groups: [] };
+    const {firstName = "", lastName = "", contactInfo, skills = []}: User = req.body;
+    const {email = "", github = ""} = contactInfo;
+    const user: User = { firstName, lastName, contactInfo: {email, github}, skills, groups: [] };
+
+    // Validates request body
+    try {
+        validateNewUser(user);
+    } catch (error) {
+        res.status(400).send({error: error.message});
+    }
     const uid: string = res.locals.user.uid;
     try {
         await createProfile(user, uid);
@@ -39,20 +48,19 @@ export const createUserProfile = async (req: Request, res: Response, next: NextF
 
 export const editUserProfile = async (req: Request, res: Response, next: NextFunction) => {
     const {firstName, lastName, contactInfo, skills}: User = req.body;
-    const user: Partial<User> = {};
-    
-    if (firstName !== undefined) {
-        user.firstName = firstName;
+    const {email, github} = contactInfo;
+    const user: Partial<User> = {
+        ...(firstName && { firstName }),
+        ...(lastName && { lastName }),
+        contactInfo: ({ ...(email && { email }), ...(github && { github })}),
+        ...(skills && { skills }),
+    };
+    try {
+        validateEdit(user);
+    } catch (error) {
+        res.status(400).send({error: error.message});
     }
-    if (lastName !== undefined) {
-        user.lastName = lastName;
-    }
-    if (contactInfo !== undefined) {
-        user.contactInfo = contactInfo;
-    }
-    if (skills !== undefined) {
-        user.skills = skills;
-    }
+
     const uid: string = res.locals.user.uid;
     try {
         await editProfile(user, uid);
