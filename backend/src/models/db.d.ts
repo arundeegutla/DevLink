52f394b3256eebefeb74fb09c93342539c33f40d
Usdiez <austinrobertson1010@gmail.com>
2023-10-26 22:02:51 -0400

    type updating

--- backend/src/models/db.d.ts
+++ backend/src/models/db.d.ts
@@ -1,10 +1,14 @@
 import { DocumentReference } from "@google-cloud/firestore";
 
 export type User = {
-  connections: DocumentReference[];
-  contactInfo: { email: string; github: string };
+  contactInfo: contactInfo;
   firstName: string;
   lastName: string;
   groups: DocumentReference[];
   skills: string[];
+};
+
+export type contactInfo = {
+  email: string;
+  github: string;
 };
