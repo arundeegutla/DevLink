2c46adcbba9b43aeeba79868a843bf1089e2a7cd
Usdiez <austinrobertson1010@gmail.com>
2023-10-26 23:27:32 -0400

    reworked getUser to be a userPage

--- backend/src/models/db.d.ts
+++ backend/src/models/db.d.ts
@@ -1,10 +1,28 @@
 import { DocumentReference } from "@google-cloud/firestore";
+import { type } from "os";
 
 export type User = {
-  email: string;
-  github: string;
+  email?: string;
+  github?: string;
   firstName: string;
   lastName: string;
   groups: DocumentReference[];
   skills: string[];
+};
+
+export type UserPage = User & {
+ groups: condensedGroup[];
+}
+
+export type condensedGroup = {
+  id: string;
+  name: string;
+  description: string;
+};
+
+export type Group = {
+  name: string;
+  description: string;
+  members: DocumentReference[];
+  posts: DocumentReference[];
 };
