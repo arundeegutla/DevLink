286df1b51f026841111d0ce17b7bc0cbfa078bce
MochaDev <michaelmcgarvey03@gmail.com>
2023-10-28 21:45:18 -0400

    Setup initial file structure for group endpoints

---
+++ backend/src/routes/group.ts
@@ -0,0 +1,128 @@
+import express, { Router } from "express";
+import { createInitialGroup, editExistingGroup } from "../controllers/group";
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - firstName
+ *         - lastName
+ *         - contactInfo
+ *         - skills
+ *       properties:
+ *         firstName:
+ *           type: string
+ *           description: First name of the user
+ *         lastName:
+ *           type: string
+ *           description: Last name of the user
+ *         email:
+ *          type: string
+ *          description: Email of the user
+ *         github:
+ *          type: string
+ *          description: Github username of the user
+ *         skills:
+ *           type: array
+ *           items:
+ *             type: string
+ *             description: skills of the user
+ *         groups:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 description: ID of the group
+ *               name:
+ *                 type: string
+ *                 description: Name of the group
+ *               description:
+ *                 type: string
+ *                 description: Description of the group
+ *       example:
+ *         firstName: John
+ *         lastName: Doe
+ *         email: johndoe@gmail.com
+ *         github: johndoe
+ *         skills: [React, Angular, Javascript, Typescript]
+ *         groups: [{ id: "123", name: "My Group", description: "A cool group!" }]
+ */
+
+// Create a new router instance
+const router: Router = express.Router();
+
+/**
+ * @swagger
+ * /user/createProfile:
+ *   post:
+ *     summary: Create a new group
+ *     description: Create a new group
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Name of the group
+ *               description:
+ *                 type: string
+ *                 description: A description of the group
+ *             example:
+ *               name: DevLink
+ *               description: This is our group
+ *     responses:
+ *       '400':
+ *         description: Bad request
+ *       '200':
+ *         description: Group created
+ *       '401':
+ *         description: Unauthorized
+ *       '403':
+ *         description: Forbidden
+ */
+router.post("/createGroup", createInitialGroup);
+
+/**
+ * @swagger
+ * /user/editGroup:
+ *   post:
+ *     summary: Edit a group
+ *     description: Edit a group's information, only pass the group's fields you want to update
+ *     requestBody:
+ *       description: Group object to update
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 required: false
+ *               description:
+ *                 type: string
+ *                 required: false
+ *             example:
+ *               name: DevLink
+ *               description: This is our group
+ *     responses:
+ *       '200':
+ *         description: Group updated successfully
+ *       '400':
+ *         description: Bad request
+ *       '401':
+ *         description: Unauthorized
+ *       '403':
+ *         description: Forbidden
+ */
+router.post("/editGroup", editExistingGroup);
+
+export default router;
