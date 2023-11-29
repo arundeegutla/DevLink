99311a292809c8bb4d0b0665a06de88881b5af2f
eternal-dx <brennangc@icloud.com>
2023-11-28 19:11:25 -0500

    updated design for chat block and messages

--- web/app/components/common/ChatMessage.tsx
+++ web/app/components/common/ChatMessage.tsx
@@ -46,7 +46,7 @@ export default function ChatMessage({
     <div className={`w-auto max-w-sm flex-col ${isOwnMessage ? "self-end" : "self-start"} mt-2`}>
       <div className="flex">
         { !isOwnMessage && <div className="mr-2 flex-shrink-0 self-end">{getProfilePic()}</div> }
-        <h1 className={`w-auto h-auto break-words ${isOwnMessage ? "bg-cyan-600" : "bg-amber-500"} rounded-xl p-2 font-medium`}>{messageContent}</h1>
+        <h1 className={`w-auto h-auto break-words ${isOwnMessage ? "bg-[#218aff]" : "bg-[#d8d8d8]"} rounded-xl p-2 font-medium`}>{messageContent}</h1>
       </div>
       { !isOwnMessage && <h1 className="font-light text-stone-200 mt-1">{user ? user.firstName + " " + user.lastName : "Unknown"}</h1> }
     </div>
--- web/app/components/common/GroupChatBlock.tsx
+++ web/app/components/common/GroupChatBlock.tsx
@@ -15,27 +15,18 @@ export default function GroupChatBlock({
   isSelected,
   changeGroup,
 }: GroupChatBlock) {
-  // const MAX_LAST_MSG_SIZE = 50;
-  // const truncateText = (text: string, size: number) => text.length <= size ? text : (text.substring(0, size - 3) + '...');
 
   return (
     // Tailwind conditional shenanigans
     <div
-      className={`w-full flex items-center duration-100 ease-in-out ${
+      className={`relative w-full flex items-center duration-100 ease-in-out ${
         isSelected ? "bg-[#1f1f1f]" : "bg-[#252525]"
-      } hover:bg-[#1f1f1f] rounded-xl p-2 mt-1`}
+      } hover:bg-[#1f1f1f] rounded-xl p-2 mt-2 border-l-[5px]`} style={{ borderColor: `${groupColor}` }}
       onClick={(e) => changeGroup(groupId)}
     >
-      <img
-        src={FaCircle}
-        className="w-10 h-10 mr-4 rounded-full"
-        alt="Group Chat Image"
-      />
       <div className="w-full min-w-0 flex flex-col break-words">
         <h1 className="text-m font-medium">{groupName}</h1>
-        {/* <h3 className="text-sm font-light text-stone-200">{truncateText(lastMessage, MAX_LAST_MSG_SIZE)}</h3> */}
       </div>
-      {/* { hasNotification && <FaCircle className="ml-2" color="crimson" /> } */}
     </div>
   );
 }
