aa4f05475582723e033114361755a07cb1d64207
SanyaW29 <SSWadhwa16@gmail.com>
2023-11-29 09:15:03 -0500

    Updated Project List

--- mobile/components/common/ProjectCard.tsx
+++ mobile/components/common/ProjectCard.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import { condensedGroup } from '../../src/hooks/models';
+import { useRouter } from 'expo-router';
 
 export default function ProjectCard(item: condensedGroup) {
   const [hovering, setIsHovering] = useState(false);
+  const router = useRouter();
+
+  const handleChatPress = () => {
+    // Navigate to the "/dev/chat" page when the chat icon is pressed
+    router.push('/dev/chat');
+  };
 
   return (
     <View
@@ -14,25 +21,43 @@ export default function ProjectCard(item: condensedGroup) {
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
-      {/* Your card contents go here */}
-      <Text style={styles.cardText}>{item.name}</Text>
+{/* Chat Icon */}
+<TouchableOpacity onPress={handleChatPress} style={styles.chatIcon}>
+        <Text style={styles.chatIconText}>💬</Text>
+      </TouchableOpacity>      <Text style={styles.cardText}>{item.name}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   cardContainer: {
-    backgroundColor: 'black',
-    padding: 10,
+    backgroundColor: 'rgba(128, 128, 128, 0.7)',
+    //backgroundColor: 'gray',
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    marginBotton: 20,
+    padding: 30,
     borderRadius: 8,
-    marginBottom: 10,
   },
   hoveredCardContainer: {
     borderColor: 'white',
-    borderWidth: 2,
+    borderWidth: 5,
   },
   cardText: {
     color: 'white',
-    fontSize: 16,
+    fontSize: 30,
+  },
+  chatIcon: {
+    position: 'absolute',
+    top: 28,
+    right: 15,
+    padding: 10,
+    backgroundColor: '#051094',
+    borderRadius: 25,
+  },
+  chatIconText: {
+    fontSize: 22,
+    color: 'white',
   },
 });
